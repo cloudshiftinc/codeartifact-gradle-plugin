7da70ca88af5e5f41478bd99c1a3b1243c2992cb
Chris Lee <chris@cloudshiftconsulting.com>
2024-02-21 16:27:57 -0800

    Release version 0.6.3

--- src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactPlugin.kt
+++ src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactPlugin.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 import net.pearx.kasechange.toPascalCase
 import org.gradle.api.Action
 import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.ArtifactRepository
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
@@ -15,6 +16,7 @@ import org.gradle.api.initialization.Settings
 import org.gradle.api.internal.artifacts.repositories.DefaultMavenArtifactRepository
 import org.gradle.api.logging.Logging
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.plugins.PluginAware
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderFactory
@@ -22,6 +24,7 @@ import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.newInstance
@@ -29,26 +32,45 @@ import org.gradle.kotlin.dsl.of
 import org.gradle.kotlin.dsl.withType
 
 public abstract class CodeArtifactPlugin @Inject constructor(private val objects: ObjectFactory) :
-    Plugin<Settings> {
+    Plugin<PluginAware> {
     private val logger = Logging.getLogger(CodeArtifactPlugin::class.java)
 
-    override fun apply(settings: Settings): Unit =
-        settings.run {
-            dependencyResolutionManagement.repositories.all {
+    override fun apply(target: PluginAware): Unit {
+        when (target) {
+            is Project -> applyToProject(target)
+            is Settings -> applyToSettings(target)
+            else -> error("CodeArtifactPlugin is not compatible with ${target.javaClass.simpleName}")
+        }
+    }
+
+    private fun applyToSettings(settings: Settings) {
+//        settings.pluginManagement.repositories.all {
+//            configureCodeArtifactRepository(this, settings.providers)
+//        }
+
+        settings.dependencyResolutionManagement.repositories.all {
+            configureCodeArtifactRepository(this, settings.providers)
+        }
+
+        settings.gradle.beforeProject {
+            buildscript.repositories.all {
                 configureCodeArtifactRepository(this, providers)
             }
+            pluginManager.apply(CodeArtifactPlugin::class)
+        }
+    }
 
-            gradle.beforeProject {
-                buildscript.repositories.all {
-                    configureCodeArtifactRepository(this, providers)
-                }
-                plugins.withType<MavenPublishPlugin> {
-                    configure<PublishingExtension> {
-                        repositories.all { configureCodeArtifactRepository(this, providers) }
-                    }
-                }
+    private fun applyToProject(project: Project) {
+        project.repositories.all {
+            configureCodeArtifactRepository(this, project.providers)
+        }
+
+        project.plugins.withType<MavenPublishPlugin> {
+            project.configure<PublishingExtension> {
+                repositories.all { configureCodeArtifactRepository(this, project.providers) }
             }
         }
+    }
 
     private fun configureCodeArtifactRepository(
         repository: ArtifactRepository,
@@ -68,6 +90,7 @@ public abstract class CodeArtifactPlugin @Inject constructor(private val objects
                     }
                 repository.setConfiguredCredentials(createRepoCredentials(tokenProvider))
             }
+
             else -> {}
         }
     }
@@ -100,11 +123,13 @@ internal abstract class RepositoryCredentials : PasswordCredentials {
 
     @get:Internal abstract val passwordProp: Property<String>
 
-    @Input override fun getUsername(): String = usernameProp.get()
+    @Input
+    override fun getUsername(): String = usernameProp.get()
 
     override fun setUsername(name: String?) = usernameProp.set(name)
 
-    @Internal override fun getPassword(): String = passwordProp.get()
+    @Internal
+    override fun getPassword(): String = passwordProp.get()
 
     override fun setPassword(pwd: String?) = passwordProp.set(pwd)
 }
