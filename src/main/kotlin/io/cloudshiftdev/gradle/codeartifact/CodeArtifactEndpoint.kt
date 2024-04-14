83e9ee2ecd95f31c1c0f573905859e3ee73bf092
Chris Lee <chris@cloudshiftconsulting.com>
2024-04-14 10:43:06 -0700

    Release 0.7.1

--- src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactEndpoint.kt
+++ src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactEndpoint.kt
@@ -1,21 +1,19 @@
 package io.cloudshiftdev.gradle.codeartifact
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import java.io.Serializable
 import java.net.URI
+import net.pearx.kasechange.toPascalCase
 
-internal data class CodeArtifactEndpoint(
-    val domain: String,
-    val domainOwner: String,
-    val region: String,
-    val repository: String,
-    val url: URI
-) : Serializable {
-    @get:JsonIgnore
-    val cacheKey: String
-        get() = url.toString()
+public interface CodeArtifactEndpoint {
+    public val domain: String
+    public val domainOwner: String
+    public val region: String
+    public val repository: String
+    public val url: URI
+    public val name: String
+        get() = "${domain}-${repository}".toPascalCase()
 
-    companion object {
+    public companion object {
         private fun fromUrl(url: String): CodeArtifactEndpoint? {
             return fromUrl(URI(url))
         }
@@ -23,7 +21,7 @@ internal data class CodeArtifactEndpoint(
         private fun fromUrl(url: URI): CodeArtifactEndpoint? {
             val urlString = url.toString()
             val match = regex.matchEntire(urlString) ?: return null
-            return CodeArtifactEndpoint(
+            return CodeArtifactEndpointImpl(
                 match.groups["domain"]!!.value,
                 match.groups["domainOwner"]!!.value,
                 match.groups["region"]!!.value,
@@ -32,18 +30,29 @@ internal data class CodeArtifactEndpoint(
             )
         }
 
-        fun URI.toCodeArtifactEndpointOrNull(): CodeArtifactEndpoint? = fromUrl(this)
+        public fun URI.toCodeArtifactEndpointOrNull(): CodeArtifactEndpoint? = fromUrl(this)
 
-        fun URI.toCodeArtifactEndpoint(): CodeArtifactEndpoint =
+        public fun URI.toCodeArtifactEndpoint(): CodeArtifactEndpoint =
             toCodeArtifactEndpointOrNull() ?: error("Invalid CodeArtifact endpoint: $this")
 
-        fun String.toCodeArtifactEndpoint(): CodeArtifactEndpoint =
+        public fun String.toCodeArtifactEndpoint(): CodeArtifactEndpoint =
             toCodeArtifactEndpointOrNull() ?: error("Invalid CodeArtifact endpoint: $this")
 
-        fun String.toCodeArtifactEndpointOrNull(): CodeArtifactEndpoint? = fromUrl(this)
+        public fun String.toCodeArtifactEndpointOrNull(): CodeArtifactEndpoint? = fromUrl(this)
 
         private val regex =
             """^https://(?<domain>.*?)-(?<domainOwner>.*?).d.codeartifact.(?<region>.+?).amazonaws.com/.+?/(?<repository>.+?)(?:/|\?.*|/\?.*)?$"""
                 .toRegex()
     }
 }
+
+internal val CodeArtifactEndpoint.cacheKey: String
+    get() = url.toString()
+
+internal data class CodeArtifactEndpointImpl(
+    override val domain: String,
+    override val domainOwner: String,
+    override val region: String,
+    override val repository: String,
+    override val url: URI
+) : Serializable, CodeArtifactEndpoint
