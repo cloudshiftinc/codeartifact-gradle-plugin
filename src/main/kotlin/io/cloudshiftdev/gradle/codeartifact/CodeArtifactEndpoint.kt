3e65c755dd5786e598a588c7b1c609b2f728ee1c
Chris Lee <chris@cloudshiftconsulting.com>
2024-04-29 11:37:12 -0700

    Release 0.7.5

--- src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactEndpoint.kt
+++ src/main/kotlin/io/cloudshiftdev/gradle/codeartifact/CodeArtifactEndpoint.kt
@@ -48,7 +48,7 @@ public interface CodeArtifactEndpoint {
 }
 
 internal val CodeArtifactEndpoint.cacheKey: String
-    get() = url.toString()
+    get() = "${domain}-${domainOwner}-${region}"
 
 internal data class CodeArtifactEndpointImpl(
     override val domain: String,
